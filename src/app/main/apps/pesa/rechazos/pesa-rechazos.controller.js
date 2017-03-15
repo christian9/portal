dc80ee47a76481202e349818a457a9025ee3222b
David Solano <desolanom@gmail.com>
2017-03-15 10:08:41 -0600

    Rechazos V2.1

--- src/app/main/apps/pesa/rechazos/pesa-rechazos.controller.js
+++ src/app/main/apps/pesa/rechazos/pesa-rechazos.controller.js
@@ -15,6 +15,7 @@
         vm.pesaData = PesaData;
         vm.colors = ['blue-bg', 'blue-grey-bg', 'orange-bg', 'pink-bg', 'purple-bg'];
 
+        //Widget 1
         vm.widget1 = {
             title             : vm.pesaData.widget1.title,
             onlineUsers       : vm.pesaData.widget1.onlineUsers,
@@ -86,10 +87,16 @@
                 },
                 data   : vm.pesaData.widget1.bigChart.chart
             },
+            
+        };
+        ////
+        //Widget 2
+        vm.widget2 = {
+            title             : vm.pesaData.widget2.title,
             sessions          : {
-                title   : vm.pesaData.widget1.sessions.title,
-                value   : vm.pesaData.widget1.sessions.value,
-                previous: vm.pesaData.widget1.sessions.previous,
+                title   : vm.pesaData.widget2.sessions.title,
+                value   : vm.pesaData.widget2.sessions.value,
+                previous: vm.pesaData.widget2.sessions.previous,
                 options : {
                     chart: {
                         type                   : 'lineChart',
@@ -132,12 +139,12 @@
                         }
                     }
                 },
-                data    : vm.pesaData.widget1.sessions.chart
+                data    : vm.pesaData.widget2.sessions.chart
             },
             pageviews         : {
-                title   : vm.pesaData.widget1.pageviews.title,
-                value   : vm.pesaData.widget1.pageviews.value,
-                previous: vm.pesaData.widget1.pageviews.previous,
+                title   : vm.pesaData.widget2.pageviews.title,
+                value   : vm.pesaData.widget2.pageviews.value,
+                previous: vm.pesaData.widget2.pageviews.previous,
                 options : {
                     chart: {
                         type                   : 'lineChart',
@@ -180,12 +187,12 @@
                         }
                     }
                 },
-                data    : vm.pesaData.widget1.pageviews.chart
+                data    : vm.pesaData.widget2.pageviews.chart
             },
             pagesSessions     : {
-                title   : vm.pesaData.widget1.pagesSessions.title,
-                value   : vm.pesaData.widget1.pagesSessions.value,
-                previous: vm.pesaData.widget1.pagesSessions.previous,
+                title   : vm.pesaData.widget2.pagesSessions.title,
+                value   : vm.pesaData.widget2.pagesSessions.value,
+                previous: vm.pesaData.widget2.pagesSessions.previous,
                 options : {
                     chart: {
                         type                   : 'lineChart',
@@ -228,12 +235,12 @@
                         }
                     }
                 },
-                data    : vm.pesaData.widget1.pagesSessions.chart
+                data    : vm.pesaData.widget2.pagesSessions.chart
             },
             avgSessionDuration: {
-                title   : vm.pesaData.widget1.avgSessionDuration.title,
-                value   : vm.pesaData.widget1.avgSessionDuration.value,
-                previous: vm.pesaData.widget1.avgSessionDuration.previous,
+                title   : vm.pesaData.widget2.avgSessionDuration.title,
+                value   : vm.pesaData.widget2.avgSessionDuration.value,
+                previous: vm.pesaData.widget2.avgSessionDuration.previous,
                 options : {
                     chart: {
                         type                   : 'lineChart',
@@ -283,13 +290,214 @@
                         }
                     }
                 },
-                data    : vm.pesaData.widget1.avgSessionDuration.chart
+                data    : vm.pesaData.widget2.avgSessionDuration.chart
             }
         };
-
-        // Widget 4
-        vm.widget4 = vm.pesaData.widget4;
-
+        ////
+        //Widget 3
+        vm.widget3 = {
+            title             : vm.pesaData.widget3.title,
+            sessions          : {
+                title   : vm.pesaData.widget3.sessions.title,
+                value   : vm.pesaData.widget3.sessions.value,
+                previous: vm.pesaData.widget3.sessions.previous,
+                options : {
+                    chart: {
+                        type                   : 'lineChart',
+                        color                  : ['#03A9F4'],
+                        height                 : 40,
+                        margin                 : {
+                            top   : 4,
+                            right : 4,
+                            bottom: 4,
+                            left  : 4
+                        },
+                        isArea                 : true,
+                        interpolate            : 'cardinal',
+                        clipEdge               : true,
+                        duration               : 500,
+                        showXAxis              : false,
+                        showYAxis              : false,
+                        showLegend             : false,
+                        useInteractiveGuideline: true,
+                        x                      : function (d)
+                        {
+                            return d.x;
+                        },
+                        y                      : function (d)
+                        {
+                            return d.y;
+                        },
+                        xAxis                  : {
+                            tickFormat: function (d)
+                            {
+                                var date = new Date(new Date().setDate(new Date().getDate() + d));
+                                return d3.time.format('%A, %B %d, %Y')(date);
+                            }
+                        },
+                        interactiveLayer       : {
+                            tooltip: {
+                                gravity: 's',
+                                classes: 'gravity-s'
+                            }
+                        }
+                    }
+                },
+                data    : vm.pesaData.widget3.sessions.chart
+            },
+            pageviews         : {
+                title   : vm.pesaData.widget3.pageviews.title,
+                value   : vm.pesaData.widget3.pageviews.value,
+                previous: vm.pesaData.widget3.pageviews.previous,
+                options : {
+                    chart: {
+                        type                   : 'lineChart',
+                        color                  : ['#3F51B5'],
+                        height                 : 40,
+                        margin                 : {
+                            top   : 4,
+                            right : 4,
+                            bottom: 4,
+                            left  : 4
+                        },
+                        isArea                 : true,
+                        interpolate            : 'cardinal',
+                        clipEdge               : true,
+                        duration               : 500,
+                        showXAxis              : false,
+                        showYAxis              : false,
+                        showLegend             : false,
+                        useInteractiveGuideline: true,
+                        x                      : function (d)
+                        {
+                            return d.x;
+                        },
+                        y                      : function (d)
+                        {
+                            return d.y;
+                        },
+                        xAxis                  : {
+                            tickFormat: function (d)
+                            {
+                                var date = new Date(new Date().setDate(new Date().getDate() + d));
+                                return d3.time.format('%A, %B %d, %Y')(date);
+                            }
+                        },
+                        interactiveLayer       : {
+                            tooltip: {
+                                gravity: 's',
+                                classes: 'gravity-s'
+                            }
+                        }
+                    }
+                },
+                data    : vm.pesaData.widget3.pageviews.chart
+            },
+            pagesSessions     : {
+                title   : vm.pesaData.widget3.pagesSessions.title,
+                value   : vm.pesaData.widget3.pagesSessions.value,
+                previous: vm.pesaData.widget3.pagesSessions.previous,
+                options : {
+                    chart: {
+                        type                   : 'lineChart',
+                        color                  : ['#E91E63'],
+                        height                 : 40,
+                        margin                 : {
+                            top   : 4,
+                            right : 4,
+                            bottom: 4,
+                            left  : 4
+                        },
+                        isArea                 : true,
+                        interpolate            : 'cardinal',
+                        clipEdge               : true,
+                        duration               : 500,
+                        showXAxis              : false,
+                        showYAxis              : false,
+                        showLegend             : false,
+                        useInteractiveGuideline: true,
+                        x                      : function (d)
+                        {
+                            return d.x;
+                        },
+                        y                      : function (d)
+                        {
+                            return d.y;
+                        },
+                        xAxis                  : {
+                            tickFormat: function (d)
+                            {
+                                var date = new Date(new Date().setDate(new Date().getDate() + d));
+                                return d3.time.format('%A, %B %d, %Y')(date);
+                            }
+                        },
+                        interactiveLayer       : {
+                            tooltip: {
+                                gravity: 's',
+                                classes: 'gravity-s'
+                            }
+                        }
+                    }
+                },
+                data    : vm.pesaData.widget3.pagesSessions.chart
+            },
+            avgSessionDuration: {
+                title   : vm.pesaData.widget3.avgSessionDuration.title,
+                value   : vm.pesaData.widget3.avgSessionDuration.value,
+                previous: vm.pesaData.widget3.avgSessionDuration.previous,
+                options : {
+                    chart: {
+                        type                   : 'lineChart',
+                        color                  : ['#009688'],
+                        height                 : 40,
+                        margin                 : {
+                            top   : 4,
+                            right : 4,
+                            bottom: 4,
+                            left  : 4
+                        },
+                        isArea                 : true,
+                        interpolate            : 'cardinal',
+                        clipEdge               : true,
+                        duration               : 500,
+                        showXAxis              : false,
+                        showYAxis              : false,
+                        showLegend             : false,
+                        useInteractiveGuideline: true,
+                        x                      : function (d)
+                        {
+                            return d.x;
+                        },
+                        y                      : function (d)
+                        {
+                            return d.y;
+                        },
+                        xAxis                  : {
+                            tickFormat: function (d)
+                            {
+                                var date = new Date(new Date().setDate(new Date().getDate() + d));
+                                return d3.time.format('%A, %B %d, %Y')(date);
+                            }
+                        },
+                        yAxis                  : {
+                            tickFormat: function (d)
+                            {
+                                var formatTime = d3.time.format('%M:%S');
+                                return formatTime(new Date('2012', '0', '1', '0', '0', d));
+                            }
+                        },
+                        interactiveLayer       : {
+                            tooltip: {
+                                gravity: 's',
+                                classes: 'gravity-s'
+                            }
+                        }
+                    }
+                },
+                data    : vm.pesaData.widget3.avgSessionDuration.chart
+            }
+        };
+        ////
 
         // Methods
 
